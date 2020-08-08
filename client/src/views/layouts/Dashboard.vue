d2532b20c289b8f1e7ddce82f9d129ca5d1219a1
ariel fisher <ariel@vcita.com>
2020-08-08 13:47:32 +0300

    Add split pane to tests dashboard

--- client/src/views/layouts/Dashboard.vue
+++ client/src/views/layouts/Dashboard.vue
@@ -1,20 +1,26 @@
 <template>
-    <div class="dashboard-main">
-        <div class="controls">
-            <slot></slot>
-        </div>
-        <div class="command">
-            <CommandlineOutput :output="output" />
-        </div>
-    </div>
+    <splitPane class="dashboard-main" :split="'horizontal'">
+        <template slot="paneL">
+            <div class="controls">
+                <slot></slot>
+            </div>
+        </template>
+        <template slot="paneR">
+            <div class="command">
+                <CommandlineOutput :output="output" />
+            </div>
+        </template>
+    </splitPane>
 </template>
 
 <script>
+import splitPane from 'vue-splitpane'
 import CommandlineOutput from '../components/CommandlineOutput';
 
 export default {
     components: {
-        CommandlineOutput
+        CommandlineOutput,
+        splitPane
     },
     props: {
         output: {type: String, default: "Nothing to show yet"},
@@ -26,16 +32,39 @@ export default {
 .dashboard-main{
     height: 100%;
     width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
 
     .controls{
         overflow-y: auto;
+        max-height: 100%;
+    }
+
+    .splitter-pane-resizer {
+        margin: 0; left: 0;
+        position: relative;
+        height: 40px;
+        &:before {
+            display: block;
+            content: "";
+            width: 40px;
+            height: 3px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-top: -1.5px;
+            margin-left: -20px;
+            border-top: 1px solid #ccc;
+            border-bottom: 1px solid #ccc;
+        }
+        &:hover {
+            &:before {
+            border-color: #999;
+            }
+        }
     }
 
     .command{
-        max-height: 50%;
+        overflow: auto;
+        max-height: 100%;
     }
 }
 </style>

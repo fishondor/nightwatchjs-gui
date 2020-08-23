11df1df976f4b69297d0b0e4de4079156a984462
Ariel Fisher <fishondor@gmail.com>
2020-08-24 00:00:08 +0300

    Fix container style

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <VueShowdown :markdown="fileContent"></VueShowdown>
+  <v-container fluid class="about">
+    <VueShowdown :markdown="fileContent" flavor="github"></VueShowdown>
+  </v-container>
 </template>
 
 <script>
@@ -25,3 +27,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.about{
+  padding-bottom: 50px;
+}
+</style>

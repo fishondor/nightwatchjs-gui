ec7cd4d1c658e57d4bc4b7d690a8d6c70b84ff53
Ariel Fisher <fishondor@gmail.com>
2020-08-29 14:02:41 +0300

    Add loader component

---
+++ client/src/views/components/Loader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="showLoader" class="loader-container">
+        <v-progress-circular
+            :size="50"
+            dark
+            indeterminate
+        ></v-progress-circular>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+    computed: mapState({
+        showLoader: state => state.showLoader
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.loader-container{
+    width: 100vw;
+    height: 100vh;
+    background: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    z-index: 1;
+}
+</style>

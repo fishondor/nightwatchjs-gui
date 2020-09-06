16e5b735c2a34e23913a9bb780b8e48ea267dad6
Ariel Fisher <fishondor@gmail.com>
2020-09-07 00:32:20 +0300

    Add reports routes

--- server/app/api.js
+++ server/app/api.js
@@ -3,15 +3,18 @@ const router = express.Router();
 
 const testsService = require('./providers/tests');
 const cronJobsService = require('./providers/cronJobs');
+const testsOutputService = require('./providers/testsOutput');
 
-router.get( '/groups', testsService.api.getTestsTreeView);
-router.get( '/tests', testsService.api.getTestsTreeView);
-router.get( '/tests-environments', testsService.api.getTestEnvironments);
-router.post( '/run-test', testsService.api.runTest);
-router.get( '/jobs', cronJobsService.getJobs);
+router.get( '/groups', testsService.api.getTestsTreeView );
+router.get( '/tests', testsService.api.getTestsTreeView );
+router.get( '/tests-environments', testsService.api.getTestEnvironments );
+router.post( '/run-test', testsService.api.runTest );
+router.get( '/jobs', cronJobsService.getJobs );
 router.get( '/job/:action/:id', cronJobsService.jobActions );
-router.delete( '/job/:id', cronJobsService.deleteJob);
-router.post( '/job', cronJobsService.registerJob);
+router.delete( '/job/:id', cronJobsService.deleteJob );
+router.post( '/job', cronJobsService.registerJob );
+router.post( '/report', testsOutputService.api.saveReport );
+router.get( '/reports', testsOutputService.api.getReports );
 
 module.exports = router;
 

2fc9bff711e03a8218fdf43785818b85c5957de2
Ariel Fisher <fishondor@gmail.com>
2020-09-07 00:26:50 +0300

    Log error in execute command

--- server/app/providers/utils.js
+++ server/app/providers/utils.js
@@ -1,10 +1,17 @@
 var exec = require('child_process').exec;
 
+const Logger = require('./Logger');
+
+const logger = new Logger("Utils");
+
 const executeCommand = (command) => {
     console.log("Executing command", command);
     return new Promise(
         (resolve) => {
-            exec(command, function(error, stdout){ resolve(stdout) });
+            exec(command, function(error, stdout){ 
+                if(error)
+                    logger.error(error);
+                resolve(stdout) });
         }
     )
 };

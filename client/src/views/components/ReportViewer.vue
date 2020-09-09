8fcc27e32c64e2c4184156c9062caea679f0b1ec
Ariel Fisher <fishondor@gmail.com>
2020-09-09 23:29:51 +0300

    Add reports viewer and set reports table

--- client/src/views/components/ReportViewer.vue
+++ client/src/views/components/ReportViewer.vue
@@ -1,13 +1,128 @@
 <template>
-    <div>Reports viewer</div>
+    <v-container>
+        <table border="0" cellpadding="0" cellspacing="0">
+            <tr class="overview">
+            <td colspan="3" :title="browser"><strong>Browser:</strong> {{browser}}</td>
+            </tr>
+            <tr class="overview">
+            <td colspan="3"><strong>Timestamp:</strong> {{timestamp}}</td>
+            </tr>
+            <tr class="overview last">
+            <td colspan="3"><strong>Tests:</strong> {{results.tests}}<br></td>
+            </tr>
+            <tr>
+            <td class="pass"><strong>{{results.passed}}</strong> passed</td>
+            <td class="skip"><strong>{{results.errors}}</strong> errors</td>
+            <td class="fail"><strong>{{results.failed}}</strong> failures</td>
+            </tr>
+        </table>
+
+        <div v-for="(testSuite, testSuiteName) in results.modules" :key="testSuiteName">
+            <h2>{{testSuiteName}}</h2>
+            <div v-for="(test, testName) in testSuite.completed" :key="testName">
+                <h3>{{testName}}</h3>
+                <ul class="assertions">
+                    <li v-for="(assertion, index) in test.assertions" :key="`assertion_${index}`">
+                        <span v-if="assertion.failure" class="error">&#10006;</span>
+                        <span v-else class="success">&#10004;</span>
+                        <span v-html="parseAnsi(assertion.message)"></span>
+                        <span v-if="assertion.failure">{{assertion.failure}}</span>
+                        <div v-if="assertion.stacktrace" class="stacktrace">
+                            <a href="#">view stacktrace</a>
+                            <code><pre>{{assertion.stacktrace}}</pre></code>
+                        </div>
+                    </li>
+                </ul>
+                <p>
+                    <template v-if="test.failed">
+                        <span class="error"><strong>FAILED:</strong></span>
+                        <span class="error"><strong>{{test.failed}}</strong></span> assertions failed and
+                    </template>
+                    <template v-else>
+                        <span v-if="!test.failed" class="success"><strong>OK.</strong></span>
+                    </template>
+                    <span class="success"><strong>{{test.passed}}</strong></span> assertions passed. ({{test.time}}s)
+                </p>
+            </div>
+            <template v-if="testSuite.skipped">
+                <h4>skipped</h4>
+                <ul>
+                    <li v-for="(skipped, skippedKey) in testSuite.skipped" :key="skippedKey">{{skipped}}</li>
+                </ul>
+            </template>
+        </div>
+
+        <hr>
+    </v-container>
 </template>
 
 <script>
-
+import * as Convert from 'ansi-to-html';
 
 export default {
-    props: {
-        data: {type: String, default: ""}
+    data(){
+        return {
+            results: {},
+            browser: '',
+            timestamp: ''
+        }
+    },
+    created(){
+        this.results = this.$route.params.report;
+        console.log("results", this.results);
+        this.browser = this.$route.params.browser;
+        this.timestamp = this.$route.params.timestamp;
+        this.converter = new Convert();
+    },
+    methods: {
+        parseAnsi(text){
+            return this.converter.toHtml(text);
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+
+    table { width: 100%; margin-bottom: 20px; }
+
+      td {
+        padding: 7px;
+        border-top: none;
+        border-left: 1px black solid;
+        border-bottom: 1px black solid;
+        border-right: none;
+      }
+
+      td.pass { color: #003b07; background: #86e191; }
+      td.skip { color: #7d3a00; background: #ffd24a; }
+      td.fail { color: #5e0e00; background: #ff9c8a; }
+
+    tr:last-child       { border-top: 1px black solid; }
+      tr:last-child td    { border-top: 1px black solid; }
+      tr:first-child td   { border-top: 1px black solid; }
+        td:last-child       { border-right: 1px black solid; }
+
+    tr.overview td      { padding-bottom: 0px; border-bottom: none; }
+    tr.overview.last td { padding-bottom: 3px; }
+
+    ul.assertions   { list-style-type: none; }
+      span.error      { color: #AD2B2B; }
+      span.success    { color: #53891E; }
+
+    .stacktrace { display: inline; }
+      .stacktrace code { display: none; }
+
+    #nightwatch-logo {
+      position: absolute;
+      top: 20px;
+      right: 33px;
+      width: 70px;
+      height: 75px;
+      background: transparent url('http://nightwatchjs.org/img/logo-nightwatch.png') no-repeat;
+      background-size: 70px 75px;
+    }
+    .container {
+        overflow-y: auto;
+    }
+</style>
--- client/src/views/components/ReportsTable.vue
+++ client/src/views/components/ReportsTable.vue
@@ -3,20 +3,49 @@
         <v-data-table
             :headers="headers"
             :items="reports"
+            :sort-by="['createdAt']"
             item-key="_id"
         >
+        <template v-slot:item.createdAt="{ item }">
+            {{formatDate(item.createdAt)}}
+        </template>
+        <template v-slot:item.report.modules="{ item }">
+            <v-chip v-for="(test, key) in item.report.modules" 
+                :key="key" 
+                class="mr-1"
+                small
+                :color="testColor(test)">
+                {{key}}
+            </v-chip>
+        </template>
+        <template v-slot:item.view="{ item }">
+            <v-icon
+                small
+                class="mr-2"
+                @click="viewItem(item)"
+            >
+                mdi-eye
+            </v-icon>
+        </template>
         </v-data-table>
     </v-layout>
 </template>
 
 <script>
+import { format } from 'date-fns';
+
+const dateFormat = "MMM do yy H:m";
 
 export default {
     data () {
         return {
             headers: [
-                { text: 'ID', value: '_id' },
-                { text: 'Executed', value: 'createdAt' }
+                { text: 'Executed', value: 'createdAt' },
+                { text: 'Modules', value: 'report.modules' },
+                { text: 'Errors', value: 'report.errors' },
+                { text: 'Failed', value: 'report.failed' },
+                { text: 'Passed', value: 'report.passed' },
+                { text: '', value: 'view' }
             ],
             reports: []
         }
@@ -33,6 +62,16 @@ export default {
             console.log("Reports", data.reports);
             this.reports = data.reports;
         },
+        formatDate(dateString){
+            let date = new Date(dateString);
+            return format(date, dateFormat);
+        },
+        testColor(test){
+            return test.failedCount == 0 && test.errorsCount == 0 ? 'success' : 'warning'
+        },
+        viewItem(item){
+            this.$router.push({ name: 'report', params: { report: item.report, options: item.options, browser: item.browser, timestamp: item.timestamp } })
+        }
     }
 }
 </script>

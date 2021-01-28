2f8ef082166d3c0a69367bc4951719aee535dace
Ariel Fisher <fishondor@gmail.com>
2021-01-28 23:37:30 +0200

    Move Server require after Environment initiation to prevent loading default data

--- NightwatchJSGUI.js
+++ NightwatchJSGUI.js
@@ -1,10 +1,10 @@
-var Server = require('./server/app/server');
 var EnvironmentService = require('./server/app/providers/Environment');
 
 class NightwatchJSGUI{
     
     constructor(args = {}){
         EnvironmentService(args);
+        var Server = require('./server/app/server');
         let server = new Server();
         server.start();
     }
